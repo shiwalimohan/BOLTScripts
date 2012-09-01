29574c01e97469e0c9e63c9365772939cad1b711
Shiwali Mohan <shiwali.mohan@gmail.com>
2012-09-01 18:17:43 -0400

    added randomizeVerbs

---
+++ randomizeVerbs.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+@verb=("move","move-left","move-right");
+@object=(1,2,3,4);
+@location=("pantry","dishwasher","garbage","table");
+@situation=("empty","incorrect","correct");
+@instruction=("direct","indirect");
+
+
+for($i=0;$i<3;$i++){
+    print $verb[$i] . " ". $object[int(rand(4))] . " ". $location[int(rand(4))] . " " . $situation[int(rand(3))] . " " . $instruction[int(rand(2))] . "\n";
+}
+@verb=("discard");
+@location=("garbage");
+    print $verb[0] . " ". $object[int(rand(4))] . " " . $situation[int(rand(3))] . " " . $instruction[int(rand(2))] . "\n";
+
+@verb=("store");
+@location=("pantry");
+    print $verb[0] . " ". $object[int(rand(4))] . " " . $situation[int(rand(3))] . " " . $instruction[int(rand(2))] . "\n";
