e65246ee0ab72d5caf30f1f5c7c4107e68cb25b1
Shiwali Mohan <shiwali.mohan@gmail.com>
2012-09-01 19:59:03 -0400

    updated randomVerb script

--- randomizeVerbs.pl
+++ randomizeVerbs.pl
@@ -3,17 +3,17 @@
 @verb=("move","move-left","move-right");
 @object=(1,2,3,4);
 @location=("pantry","dishwasher","garbage","table");
-@situation=("empty","incorrect","correct");
+@situation=("empty","correct");
 @instruction=("direct","indirect");
 
 
 for($i=0;$i<3;$i++){
-    print $verb[$i] . " ". $object[int(rand(4))] . " ". $location[int(rand(4))] . " " . $situation[int(rand(3))] . " " . $instruction[int(rand(2))] . "\n";
+    print $verb[$i] . " ". $object[int(rand(4))] . " ". $location[int(rand(4))] . " " . $situation[int(rand(2))] . " " . $instruction[int(rand(2))] . "\n";
 }
 @verb=("discard");
 @location=("garbage");
-    print $verb[0] . " ". $object[int(rand(4))] . " " . $situation[int(rand(3))] . " " . $instruction[int(rand(2))] . "\n";
+    print $verb[0] . " ". $object[int(rand(4))] . " " . $situation[int(rand(2))] . " " . $instruction[int(rand(2))] . "\n";
 
 @verb=("store");
 @location=("pantry");
-    print $verb[0] . " ". $object[int(rand(4))] . " " . $situation[int(rand(3))] . " " . $instruction[int(rand(2))] . "\n";
+    print $verb[0] . " ". $object[int(rand(4))] . " " . $situation[int(rand(2))] . " " . $instruction[int(rand(2))] . "\n";
